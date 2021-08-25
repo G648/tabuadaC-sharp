5e1b0f6dfb9c2c9b615214128e252761bcf109bb
Guilherme_Amorim <cezarguilherme03@gmail.com>
2021-08-25 14:33:45 -0300

    fazendo o projeto calculadora com Switch Case

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("digite o primeiro numero: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("qual opreração voce deseja fazer ? (+ - / *)");
+            string opreração = Console.ReadLine();
+            //declarando uma variavel para a futura condição;
+
+            Console.WriteLine("digite outro numero para a operação: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double resultado = 0;
+            bool validador = true;
+
+
+           switch (opreração){
+                case "+":
+                    resultado = num1 + num2;
+                break;
+                case "-":
+                    resultado = num1 - num2;
+                break;
+                case "*":
+                    resultado = num1 * num2;
+                break;
+                case "/":
+                    resultado = num1 / num2;
+                break;
+                default:
+                    Console.WriteLine("operação invalida");
+                    validador = false;
+                break;
+           }
+
+            if(validador == true){
+           Console.WriteLine("o resultado é = "+resultado);
+            }
+
+        }//fim da Main
+    }
+}
